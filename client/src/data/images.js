962194c27a3455b9b52440887fbd3372dd608c71
marvin <okongomarvin971@gmail.com>
2024-10-09 22:12:45 +0300

    changes

---
+++ client/src/data/images.js
@@ -0,0 +1,8 @@
+const images = [
+    { src: require('../images/abtImg1.jpg'), alt: 'Description 1', marginTop: '2rem' },
+    { src: require('../images/abtImg2.jpg'), alt: 'Description 2', marginTop: '7rem' },
+    { src: require('../images/abtImg3.jpg'), alt: 'Description 3', marginTop: '5rem' },
+    { src: require('../images/abtImg4.png'), alt: 'Description 4', marginTop: '1rem' },
+    { src: require('../images/abtImg5.jpg'), alt: 'Description 5', marginTop: '6rem' },
+  ];
+  
